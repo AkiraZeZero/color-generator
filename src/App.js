dcf7d144d6676c3c5b5bf653ebc328b41d099d70
AkiraZeZero <kiara.ocasio07@gmail.com>
2024-02-01 17:53:59 -0500

    css set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const colorPicker = () => {
+    const randomColor =
+    Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    colorPicker.innerHTML = "#" + randomColor
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <div className="colorCard">
+        <div className="color"></div>
+        <div className="bottomCard"></div>
+      </div>
+      <div className="button">
+        <button onClick={colorPicker} className="btn">New Color!</button>
+      </div>  
+      </div>
     </div>
   );
 }
